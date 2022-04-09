3a129b40882e099c9a8dabdde83e6940be17ff3a
Stephen Miller <smarticleparticle1@gmail.com>
2022-04-09 01:42:40 -0400

    testing canvas logic

---
+++ TowerDefenseProject/src/components/objects/Canvas.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from 'react';
+import { mouse } from '../pages/GamePage';
+
+const Canvas = props => {
+
+    const { draw, ...rest } = props;
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        let canvasPos = canvas.getBoundingClientRect();
+        window.addEventListener('resize', function (e) {
+            canvasPos = canvas.getBoundingClientRect();
+        });
+
+        canvas.addEventListener('mousemove', function (e) {
+            mouse.x = e.x - canvasPos.left;
+            mouse.y = e.y - canvasPos.top;
+            //console.log((mouse.x) + ', ' + (mouse.y));
+        });
+
+        canvas.addEventListener('mouseleave', function (e) {
+            mouse.x = undefined;
+            mouse.y = undefined;
+        });
+
+        draw(ctx);
+        //drawGrid(ctx);
+        let animationFrameID;
+
+        const render = () => {
+            draw(ctx);
+            animationFrameID = window.requestAnimationFrame(render);
+        }
+        render();
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameID);
+            window.removeEventListener('resize', function (e) {
+                canvasPos = canvas.getBoundingClientRect();
+            });
+            canvas.removeEventListener('mousemove', function (e) {
+                mouse.x = e.x - canvasPos.x;
+                mouse.y = e.y - canvasPos.y;
+                //console.log((mouse.x) + ', ' + (mouse.y));
+            });
+            canvas.removeEventListener('mouseleave', function (e) {
+                mouse.x = undefined;
+                mouse.y = undefined;
+            });
+        }
+    }, [draw]);
+
+    return <canvas ref={canvasRef} {...rest} />;
+}
+
+export default Canvas;
--- TowerDefenseProject/src/components/pages/GamePage.js
+++ TowerDefenseProject/src/components/pages/GamePage.js
@@ -1,16 +1,65 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
-const GamePage = () => (
-    <div>
-        <h1>Game Page</h1>
-        <div className="container">
-            <Link to='/scores' >
-                <Button variant="outline-light">Leaderboard</Button>
-            </Link>
+import Canvas from '../objects/Canvas';
+import { Enemy } from "../objects/enemy.js";
+import { Block } from '../objects/block';
+import { collision } from '../utils/utils';
+
+import circleImg from "../objects/circle.png";
+const circle = new Image();
+circle.src = circleImg;
+
+export let towers = [];
+export let bullets = [];
+export let enemies = [];
+export let grid = [];
+
+export const mouse = {
+    x: undefined,
+    y: undefined,
+    width: .1,
+    height: .1,
+}
+
+window.addEventListener("click", function (e) {
+    console.log(mouse.x + ', ' + mouse.y);
+});
+
+function GamePage() {
+    const test = new Enemy(400, 10, 3);
+    for (let y = 0; y < 600; y += 50) {
+        for (let x = 0; x < 900; x += 50) {
+            grid.push(new Block(x, y));
+        }
+    }
+    grid.forEach(block => {
+        console.log(block.x + ', ' + block.y);
+    });
+
+    const draw = (ctx) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        grid.forEach(block => {
+            block.draw(ctx);
+        });
+        test.draw(ctx)
+        test.update();
+    }
+
+    return (
+        <div>
+            <h1>Game Page</h1>
+            <div className="container">
+                <Canvas draw={draw} width='900' height='600'/>
+            </div>
+            <div className="container">
+                <Link to='/scores' >
+                    <Button variant="outline-light">Leaderboard</Button>
+                </Link>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default GamePage;
