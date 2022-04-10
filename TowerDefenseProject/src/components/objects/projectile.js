48c9c4e4e5589208719bc31d588b0ea817e75e83
Stephen Miller <smarticleparticle1@gmail.com>
2022-04-10 06:00:11 -0400

    tower firing logic

--- TowerDefenseProject/src/components/objects/projectile.js
+++ TowerDefenseProject/src/components/objects/projectile.js
@@ -1,13 +1,19 @@
 
+import circleImg from "./circle.png";
+const circle = new Image();
+circle.src = circleImg;
+
 export function Projectile(x, y, type) {
     this.x = x;
     this.y = y;
+    this.width = 5;
+    this.height = 5;
     this.type = type;
     if (this.type === 1) {
 
     }
     else {
-        this.speed = 5;
+        this.speed = 3;
         this.pwr = 1;
 
 
@@ -16,7 +22,7 @@ export function Projectile(x, y, type) {
 
 Projectile.prototype = {
     draw: function (ctx) {
-
+        ctx.drawImage(circle, this.x, this.y, this.width, this.height);
     },
     update: function () {
         this.x += this.speed;
--- TowerDefenseProject/src/components/objects/tower.js
+++ TowerDefenseProject/src/components/objects/tower.js
@@ -1,22 +1,32 @@
 
+import { Projectile } from './projectile';
+import circleImg from "./circle.png";
+const circle = new Image();
+circle.src = circleImg;
+
+
+
 export function Tower(x, y, type) {
     this.x = x;
     this.y = y;
     this.type = type;
-    this.width = 60;
-    this.height = 60;
-    if (this.type === placeholder) {
+    this.width = 50;
+    this.height = 50;
+    this.fire = false;
+    if (this.type === 1) {
         this.health = 3;
         this.fireRate = 2;
-        this.projectile = placeholder;
+        this.projectile = 6;
     }
 }
 
 Tower.prototype = {
     draw: function (ctx) {
-
+        ctx.drawImage(circle, this.x, this.y);
     },
-    update: function () {
-
+    shoot: function (pList) {
+        if (this.fire = true) {
+            pList.push(new Projectile(this.x + this.width, this.y + this.height / 2));
+        }
     }
 }
