f001629fcc7917508a5295809a6524ce732dea78
Stephen Miller <smarticleparticle1@gmail.com>
2022-04-14 22:29:23 -0400

    draw map

--- TowerDefenseProject/src/components/objects/block.js
+++ TowerDefenseProject/src/components/objects/block.js
@@ -1,20 +1,24 @@
 import { useEffect } from 'react';
-import { mouse, towers } from '../pages/GamePage';
+import { mouse, towers, towerType } from '../pages/GamePage';
 import { collision } from '../utils/utils';
 import { Tower } from '../objects/tower';
 
 
 
-export function Block(x, y) {
+export function Block(x, y, type) {
     this.x = x;
     this.y = y;
+    this.type = type;
     this.width = 50;
     this.height = 50;
+    this.hasTower = false;
 
     useEffect(() => {
         const place = () => {
-            if (mouse.x && mouse.y && collision(this, mouse)) {
-                towers.push(new Tower(this.x, this.y, 4));
+            if (mouse.x && mouse.y && collision(this, mouse) && this.type != 1 && !this.hasTower) {
+                towers.push(new Tower(this.x, this.y, towerType));
+                this.hasTower = true;
+                //console.log(towers.length);
             }
         }
         window.addEventListener('click', place);
@@ -26,9 +30,15 @@ export function Block(x, y) {
 
 Block.prototype = {
     draw: function (ctx) {
-        ctx.strokeStyle = 'white';
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        if (mouse.x && mouse.y && collision(this, mouse)) {
+        if (this.type === 0) {
+            ctx.strokeStyle = 'white';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        } else {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+        
+        if (mouse.x && mouse.y && collision(this, mouse) && this.type != 1) {
             ctx.fillStyle = "rgba(255, 255, 255, .5)";
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
