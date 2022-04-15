a6205959219da138bab1dac4438d94ae66354f60
Stephen Miller <smarticleparticle1@gmail.com>
2022-04-14 22:28:17 -0400

    initial shooting logic

--- TowerDefenseProject/src/components/objects/projectile.js
+++ TowerDefenseProject/src/components/objects/projectile.js
@@ -9,14 +9,13 @@ export function Projectile(x, y, type) {
     this.width = 5;
     this.height = 5;
     this.type = type;
+    this.end = false;
     if (this.type === 1) {
 
     }
     else {
-        this.speed = 3;
+        this.speed = 8;
         this.pwr = 1;
-
-
     }
 }
 
@@ -24,8 +23,17 @@ Projectile.prototype = {
     draw: function (ctx) {
         ctx.drawImage(circle, this.x, this.y, this.width, this.height);
     },
-    update: function () {
-        this.x += this.speed;
+    move: function (target) {
+        let distX = (target.x + target.width/2) - this.x;
+        let distY = (target.y + target.height/2) - this.y;
+        let angle = Math.atan2(distY, distX);
+
+        this.x += this.speed * Math.cos(angle);
+        this.y += this.speed * Math.sin(angle);
+
+        if ((distX < 0 ? -distX : distX) + (distY < 0 ? -distY : distY) < 2) {
+            this.end = true;
+        }
     }
 
 }
--- TowerDefenseProject/src/components/objects/tower.js
+++ TowerDefenseProject/src/components/objects/tower.js
@@ -12,10 +12,12 @@ export function Tower(x, y, type) {
     this.type = type;
     this.width = 50;
     this.height = 50;
-    this.fire = false;
+    this.timer = Date.now();
+    this.fire = true;
+    this.fireRate = 2;
     if (this.type === 1) {
-        this.health = 3;
-        this.fireRate = 2;
+        this.range = 100;
+        this.fireRate = 1.5;
         this.projectile = 6;
     }
 }
@@ -25,8 +27,15 @@ Tower.prototype = {
         ctx.drawImage(circle, this.x, this.y);
     },
     shoot: function (pList) {
-        if (this.fire = true) {
-            pList.push(new Projectile(this.x + this.width, this.y + this.height / 2));
+        if (this.fire) {
+            pList.push(new Projectile(this.x + this.width / 2, this.y + this.height / 2));
+            this.fire = false;
+            this.timer = Date.now();
+        } else if ((Date.now() - this.timer) / 1000 >= this.fireRate) {
+            this.fire = true;
+        }
+        else {
+            //console.log(Math.round((Date.now() - this.timer) / 1000));
         }
     }
 }
