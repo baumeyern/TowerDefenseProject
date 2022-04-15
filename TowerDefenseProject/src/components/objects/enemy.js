fe84e556efe754daa607be1804fab8c4d592ce31
Stephen Miller <smarticleparticle1@gmail.com>
2022-04-14 22:29:33 -0400

    enemy pathing

--- TowerDefenseProject/src/components/objects/enemy.js
+++ TowerDefenseProject/src/components/objects/enemy.js
@@ -1,4 +1,4 @@
-
+//import { path } from '../pages/GamePage';
 import circleImg from "./circle.png";
 const circle = new Image();
 circle.src = circleImg;
@@ -8,6 +8,8 @@ export function Enemy(x, y, type) {
     this.x = x;
     this.y = y;
     this.type = type;
+    this.waypoint = 0;
+    this.end = false
     if (this.type === 1) {
 
     }
@@ -24,7 +26,21 @@ Enemy.prototype = {
     draw: function (ctx) {
         ctx.drawImage(circle, this.x, this.y);
     },
-    update: function () {
-        this.x -= this.speed;
+    move: function (path) {
+        if (!this.end) {
+            let distX = path[this.waypoint].x - this.x;
+            let distY = path[this.waypoint].y - this.y;
+            let angle = Math.atan2(distY, distX);
+
+            this.x += this.speed * Math.cos(angle);
+            this.y += this.speed * Math.sin(angle);
+        }
+        if (this.x === path[this.waypoint].x && this.y === path[this.waypoint].y) {
+            this.waypoint++;
+        }
+        if (this.waypoint >= path.length) {
+            this.end = true;
+        }
+
     }
 }
